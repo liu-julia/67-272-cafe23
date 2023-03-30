c9611336efafe6c467490c57aab688bbdc172ce2
Julia Liu <julialiu@andrew.cmu.edu>
2023-03-30 17:50:43 -0400

    tweaked routes again

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   scope module: 'api', defaults: {format: 'json'} do
     namespace :v1 do
       # provide the routes for the API here
-      get 'shifts/:id', to: 'shifts#show', as: :shifts
+      get 'shifts/:id', to: 'shifts#show', as: :shift
       # resources :shifts
       get 'employees', to: 'employees#index', as: :employees
       get 'stores/:id/:upcoming', to: 'stores#show', as: :upcoming_shifts
